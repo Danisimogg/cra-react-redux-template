efa968e6d56073fcc730fbfb4feb6e5a9786f3d7
Danisimogg <angrydaniall123456@gmail.com>
2020-08-09 00:00:15 +0300

    feat:(add remove demo script)

--- tools/removeDemo/removeDemo.js
+++ tools/removeDemo/removeDemo.js
@@ -1,8 +1,14 @@
 // This script removes demo app files
 let rimraf = require('rimraf');
 let fs = require('fs');
-let {chalkSuccess} = require('./chalkConfig');
+let chalk = require('chalk');
+// let replace = require("replace");
+let prompt = require("prompt");
+// let prompts = require('./setupPrompts');
 
+let chalkSuccess = chalk.green;
+let chalkProcessing = chalk.blue;
+// let chalkWarn = chalk.red;
 
 /* eslint-disable no-console */
 
@@ -60,18 +66,36 @@ function removePackageJsonScriptEntry(scriptName) {
   fs.writeFileSync(packageJsonPath, JSON.stringify(content, null, 2) + '\n');
 }
 
-let numPathsRemoved = 0;
-pathsToRemove.map(path => {
-  removePath(path, () => {
-    numPathsRemoved++;
-    if (numPathsRemoved === pathsToRemove.length) {
-      // All paths have been processed
-      // Now we can create files since we're done deleting.
-      filesToCreate.map(file => createFile(file));
-    }
+console.log(chalkSuccess('Dependencies installed.'));
+
+prompt.start();
+
+console.log(chalkProcessing("WARNING:  Preparing to delete local git repository..."));
+prompt.get([{
+  name: 'deleteGit',
+  description: "Delete the git repository?  [Y/n]"
+}], function (err, result) {
+  let deleteGit = result.deleteGit.toUpperCase();
+
+  if (err) {
+    process.exit(1);
+  }
+  if (deleteGit.match(/^N.*/)) {
+    updatePackage();
+  }
+  let numPathsRemoved = 0;
+  pathsToRemove.map(path => {
+    removePath(path, () => {
+      numPathsRemoved++;
+      if (numPathsRemoved === pathsToRemove.length) {
+        // All paths have been processed
+        // Now we can create files since we're done deleting.
+        filesToCreate.map(file => createFile(file));
+      }
+    });
   });
+
+  removePackageJsonScriptEntry('remove-demo');
+
+  console.log(chalkSuccess('Demo app removed.'));
 });
-
-removePackageJsonScriptEntry('remove-demo');
-
-console.log(chalkSuccess('Demo app removed.'));
